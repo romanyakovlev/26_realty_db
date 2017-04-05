0d1aa994f25da27ebbd6299403034ae42bf886b6
romanyakovlev <yakovlevroman8@gmail.com>
2017-04-06 00:41:53 +0700

    add js

--- server.py
+++ server.py
@@ -15,13 +15,15 @@ def homepage(page):
 
     if request.args.get('oblast_district'):
         cache.set('oblast_district', request.args.get('oblast_district'))
+        cache.delete('min_price')
+        cache.delete('max_price')
         apartments = apartments.filter(Apartment.oblast_district == request.args.get('oblast_district'))
     else:
         oblast_district_cache = cache.get('oblast_district')
         if oblast_district_cache:
             apartments = apartments.filter(Apartment.oblast_district == oblast_district_cache)
 
-    if request.args.get('min_price'):
+    if bool(request.args.get('min_price')):
         cache.set('min_price', request.args.get('min_price'))
         apartments = apartments.filter(Apartment.price >= int(request.args.get('min_price')))
     else:
@@ -29,7 +31,8 @@ def homepage(page):
         if min_price_cache:
             apartments = apartments.filter(Apartment.oblast_district >= min_price_cache)
 
-    if request.args.get('max_price'):
+    if bool(request.args.get('max_price')):
+        print('kek')
         cache.set('max_price', request.args.get('max_price'))
         apartments = apartments.filter(Apartment.price <= int(request.args.get('max_price')))
     else:
@@ -39,6 +42,8 @@ def homepage(page):
 
     count = apartments.count()
     posts = apartments.paginate(page, POSTS_PER_PAGE, count)
+    print(request.args.get('oblast_district'), bool(request.args.get('max_price')), bool(request.args.get('min_price')))
+
     return render_template('ads_list.html', pagination=posts)
 
 
