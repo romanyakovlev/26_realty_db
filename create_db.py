3f39fd28e12fbe096a7ea79019e7a7e613cc4409
romanyakovlev <yakovlevroman8@gmail.com>
2017-07-26 03:11:59 +0700

    a

--- create_db.py
+++ create_db.py
@@ -1,5 +1,6 @@
 from main import db
 import requests
+from models import Apartment
 
 
 url_path = "https://devman.org/assets/ads.json"
@@ -12,3 +13,4 @@ def get_json_data(url_path):
 
 if __name__ == "__main__":
     db.create_all()
+    db.session.commit()
