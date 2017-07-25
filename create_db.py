5f43977dea1c1873dee64f37815d44646f7fdfd9
romanyakovlev <yakovlevroman8@gmail.com>
2017-07-25 21:43:34 +0700

    rename vars

--- create_db.py
+++ create_db.py
@@ -1,7 +1,14 @@
 from main import db
+import requests
+
+
+url_path = "https://devman.org/assets/ads.json"
+
+
+def get_json_data(url_path):
+    json_data = requests.get(url_path).json()
+    return json_data
 
 
 if __name__ == "__main__":
     db.create_all()
-    db.session.commit()
-    db.session.close()
--- server.py
+++ server.py
@@ -9,29 +9,29 @@ POSTS_PER_PAGE = 15
 @app.route('/<int:page>')
 def ads_list(page):
 
-    form = dict()
+    form_advert_info = dict()
     apartments = Apartment.query
 
     if request.args.get('oblast_district'):
-        form['oblast_district'] = request.args.get('oblast_district')
-        apartments = apartments.filter(Apartment.oblast_district == form['oblast_district'])
+        form_advert_info_advert_info['oblast_district'] = request.args.get('oblast_district')
+        apartments = apartments.filter(Apartment.oblast_district == form_advert_info['oblast_district'])
 
     if request.args.get('new_building'):
-        form['new_building'] = bool(request.args.get('new_building'))
-        apartments = apartments.filter(Apartment.active == form['new_building'])
+        form_advert_info['new_building'] = bool(request.args.get('new_building'))
+        apartments = apartments.filter(Apartment.active == form_advert_info['new_building'])
 
     if request.args.get('min_cost'):
-        form['min_cost'] = request.args.get('min_cost')
-        apartments = apartments.filter(Apartment.price >= form['min_cost'])
+        form_advert_info['min_cost'] = request.args.get('min_cost')
+        apartments = apartments.filter(Apartment.price >= form_advert_info['min_cost'])
 
     if request.args.get('max_cost'):
-        form['max_cost'] = request.args.get('max_cost')
-        apartments = apartments.filter(Apartment.price <= form['max_cost'])
+        form_advert_info['max_cost'] = request.args.get('max_cost')
+        apartments = apartments.filter(Apartment.price <= form_advert_info['max_cost'])
 
     count = apartments.count()
     posts = apartments.paginate(page, POSTS_PER_PAGE, count)
 
-    return render_template('ads_list.html', pagination=posts, form=form)
+    return render_template('ads_list.html', pagination=posts, form=form_advert_info)
 
 
 if __name__ == "__main__":
