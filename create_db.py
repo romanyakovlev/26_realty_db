feb0c2bab6948b7d685c873eeeb7165f3796c8a3
romanyakovlev <yakovlevroman8@gmail.com>
2017-08-10 04:02:40 +0700

    remove func from fill_db.py & del  db file

--- create_db.py
+++ create_db.py
@@ -3,14 +3,6 @@ import requests
 from models import Apartment
 
 
-url_path = "https://devman.org/assets/ads.json"
-
-
-def get_json_data(url_path):
-    json_data = requests.get(url_path).json()
-    return json_data
-
-
 if __name__ == "__main__":
     db.create_all()
     db.session.commit()
