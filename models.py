8e0dd3cd7f19563f70b6a946d7b38119a8fd584d
romanyakovlev <yakovlevroman8@gmail.com>
2017-08-10 03:58:52 +0700

    add apartments_filter func & change new_building calculation

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 from main import db
-from datetime import date
 
-year_limit = 2
+
 string_length = 500
 
 
@@ -21,16 +20,6 @@ class Apartment(db.Model):
     premise_area = db.Column(db.Float)
     apartment_id = db.Column(db.Integer)
     active = db.Column(db.Boolean, default=True, index=True)
-    new_building = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return self.settlement
-
-    def __init__(self, **kwargs):
-        super(Apartment, self).__init__(**kwargs)
-        current_year = date.today().year
-        if self.under_construction is True:
-            self.new_building = True
-        if isinstance(self.construction_year, int):
-            if current_year - self.construction_year <= year_limit:
-                self.new_building = True
--- server.py
+++ server.py
@@ -2,28 +2,40 @@ from flask import render_template, request
 from main import app
 from models import Apartment
 from sqlalchemy.sql import func
+from datetime import date
+
 
 POSTS_PER_PAGE = 15
+year_limit = 2
 
-def apartments_filter(form):
-    if form['']
+
+def apartments_filter(get_params, apartments):
+    if get_params.get('oblast_district'):
+        apartments = apartments.filter(Apartment.oblast_district == get_params.get('oblast_district')[0])
+    if get_params.get('min_cost'):
+        apartments = apartments.filter(Apartment.price >= get_params.get('min_cost')[0])
+    if get_params.get('max_cost'):
+        apartments = apartments.filter(Apartment.price <= get_params.get('max_cost')[0])
+    if get_params.get('new_building'):
+        current_year = date.today().year
+        apartments = apartments.filter(Apartment.under_construction == True).filter(
+                              current_year - Apartment.construction_year <= year_limit)
+
+    return apartments
+
 
 @app.route('/', defaults={'page': 1})
 @app.route('/<int:page>')
 def ads_list(page):
     apartments = Apartment.query
-
-    apartments = apartments.filter(Apartment.oblast_district == oblast_district)
-    if new_building:
-        apartments = apartments.filter(Apartment.active == new_building)
-    apartments = apartments.filter(Apartment.price >= min_cost)
-    apartments = apartments.filter(Apartment.price <= max_cost)
-    kek = dict(request.args)
+    all_get_params  = dict(request.args)
+    not_null_get_params = {param_key: all_get_params[param_key] for param_key in
+                                            all_get_params.keys() if all_get_params[param_key][0] is not ''}
+    apartments = apartments_filter(not_null_get_params, apartments)
     count = apartments.count()
     posts = apartments.paginate(page, POSTS_PER_PAGE, count)
 
-    return render_template('ads_list.html', pagination=posts,
-                                            form={k:kek[k] for k in kek.keys() if kek[k][0] is not ''})
+    return render_template('ads_list.html', pagination=posts, get_params=not_null_get_params)
 
 
 if __name__ == "__main__":
